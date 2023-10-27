522d9fb0ecb435a57bfde1ea3b9f1972d3b76847
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-10-27 15:16:22 +0100

    initial release

--- variables.tf
+++ variables.tf
@@ -1,26 +1,5 @@
-variable "partition" {
-  description = "AWS partition."
-  type        = string
-  nullable    = false
-  default     = "aws"
-}
-
-variable "account_id" {
-  description = "The account ID."
-  type        = string
-  nullable    = false
-  default     = ""
-}
-
 variable "cluster_name" {
   description = "The name of the EKS cluster that has been created."
   type        = string
   nullable    = false
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources."
-  type        = map(string)
-  nullable    = false
-  default     = {}
 }
