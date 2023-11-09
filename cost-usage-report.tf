7ec910f116253275bc2f9995286e2ffd07d42b2a
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-11-09 11:49:23 +0000

    Fix CUR

--- cost-usage-report.tf
+++ cost-usage-report.tf
@@ -4,13 +4,14 @@ resource "aws_cur_report_definition" "cur_report_definition" {
   compression                = "Parquet"
   format                     = "Parquet"
   additional_schema_elements = ["RESOURCES"]
-  s3_bucket                  = "eks-cost-usage-report-${var.account_id}"
+  s3_bucket                  = aws_s3_bucket.cost_and_usage_report.id
   s3_region                  = var.cur_athena_bucket_region
   s3_prefix                  = "opencost-prefix/opencost-report"
   additional_artifacts       = ["ATHENA"]
   report_versioning          = "OVERWRITE_REPORT"
 
   depends_on = [
-    aws_s3_bucket.athena_query_results
+    aws_s3_bucket.athena_query_results,
+    aws_s3_bucket.cost_and_usage_report
   ]
 }
