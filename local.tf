69ddf212580cd56edfe443250b740ee78ab67e69
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-11-09 23:19:43 +0000

    Add tags

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 locals {
-    cur_report_s3_bucket_region = "us-east-1"
-    cur_report_s3_prefix = "opencost-prefix/opencost-report"
+  cur_report_s3_bucket_region = "us-east-1"
+  cur_report_s3_prefix        = "opencost-prefix/opencost-report"
 }
